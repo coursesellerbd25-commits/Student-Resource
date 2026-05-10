8e345bfbc2299edbbc53a09e6084b6b5db0ea923
SultanaJahan25 <coursesellerbd25@gamil.com>
2026-05-10 12:06:37 +0600

    Customized Server.js

--- socket-server/server.js
+++ socket-server/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
 const cors = require("cors");
+const PORT = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors());
